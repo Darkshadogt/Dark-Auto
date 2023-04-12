e939c715c5a669a009eccf79c53fbcb3aedbbcba
Darkshadogt <122583206+Darkshadogt@users.noreply.github.com>
2023-04-11 21:50:43 -0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,13 +1,23 @@
+import __future__
+import ctypes
+import platform
+import ctypes.wintypes
+import queue
+import tkinter
+import tkinter.filedialog
+import distutils
+import tkinter.font
+import tkinter.ttk
+
 if __name__ == '__main__':
     if __package__ is None:
         import sys
         from os import path
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
         from classes import GUI, clicker, presser
     else:
         from ..classes import GUI, clicker, presser
     window = GUI.Interface()
     window.main()
-
 
 
